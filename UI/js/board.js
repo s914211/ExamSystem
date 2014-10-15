7c4329eebb84098e0c3e74e33a71df77ccd15341
jimxjim <xyz2222aqaq@yahoo.com.tw>
2014-10-15 19:09:39 +0800

    duck is coming...... soon! table td word-break

--- UI/js/board.js
+++ UI/js/board.js
@@ -4,7 +4,14 @@ $(document).ready(function() {
         $(this).addClass("trclick").siblings().removeClass("trclick");
         for (i = 0; i <= 4; i++) {
 
-            $('.text').eq(i).text($('.trclick > td').eq(i).text());
+            $('.text').eq(i).val($('.trclick > td').eq(i).text());
         };
     });
+
+    $('button').click(function(){
+    	for (i = 0; i <= 4; i++) {
+
+            $('.trclick > td').eq(i).text($('.text').eq(i).val());
+        };
+    })
 });
