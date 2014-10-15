7035857db4e993c2bc1e2ed4b73b7e0b78225fb5
jimxjim <xyz2222aqaq@yahoo.com.tw>
2014-10-15 16:54:31 +0800

    I am hungry,textareas are ,too

--- UI/js/board.js
+++ UI/js/board.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
         $(this).addClass("trclick").siblings().removeClass("trclick");
         for (i = 0; i <= 4; i++) {
-        	
+
             $('.text').eq(i).text($('.trclick > td').eq(i).text());
         };
     });
