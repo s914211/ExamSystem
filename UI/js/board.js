d1e0e211a2d98e9b630fb9a122d92be3e150df14
jimxjim <xyz2222aqaq@yahoo.com.tw>
2014-10-15 21:06:38 +0800

    wo hai mei hsieh how table der shin zheng

--- UI/js/board.js
+++ UI/js/board.js
@@ -4,14 +4,19 @@ $(document).ready(function() {
         $(this).addClass("trclick").siblings().removeClass("trclick");
         for (i = 0; i <= 4; i++) {
 
-            $('.text').eq(i).val($('.trclick > td').eq(i).text());
+            $('.text').eq(i).val($('.trclick > td').eq(i + 1).text());
         };
     });
 
-    $('button').click(function(){
-    	for (i = 0; i <= 4; i++) {
+    $('#editbtn').click(function() {
+        for (i = 0; i <= 4; i++) {
 
-            $('.trclick > td').eq(i).text($('.text').eq(i).val());
+            $('.trclick > td').eq(i + 1).text($('.text').eq(i).val());
         };
-    })
+    });
+    $('#newbtn').click(function() {
+        $('table').append("<tr class=\"trnormal\"><td>" + $('.trnormal').last().find('td').eq(0).text() + "</td><td>2</td><td>3</td>4<td>5</td><td>6</td><td>7</td></tr>");
+
+    });
+
 });
