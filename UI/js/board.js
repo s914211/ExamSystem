22f512e9c84c94bf4fe8d94b0d6689d224f13463
jimxjim <xyz2222aqaq@yahoo.com.tw>
2014-10-21 14:52:42 +0800

    balljung help me but it seems too fat

--- UI/js/board.js
+++ UI/js/board.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-trclick();
+$('.trnormal').on('click', function() {
+    $(this).addClass("trclick").siblings().removeClass("trclick");
+    for (i = 0; i <= 4; i++) {
+
+        $('.text').eq(i).val($('.trclick > td').eq(i).text());
+    };
+});
 
 $('#editbtn').click(function() {
     for (i = 0; i <= 4; i++) {
@@ -9,36 +15,35 @@ $('#editbtn').click(function() {
     };
 });
 
+
     $('#newbtn').click(function() {
     	$('.trnormal').removeClass('trclick');
-        $('table').append(
-        	"<tr class=\"trnormal trclick\"	onclick=\"trclick()\">" 
+    	var tr = $("<tr class=\"trnormal trclick\"	>" 
         	+ "<td>" + $('.text').eq(0).val() + "</td>" 
         	+ "<td>" + $('.text').eq(1).val() + "</td>" 
         	+ "<td>" + $('.text').eq(2).val() + "</td>" 
         	+ "<td>" + $('.text').eq(3).val() + "</td>" 
         	+ "<td>" + $('.text').eq(4).val() + "</td>" 
-        	+ "</tr>"
-        );
-    });
+        	+ "</tr>");
 
-    $('#deletebtn').click(function() {
+       	$('table').append(tr);
+ 		
+ 		tr.on('click', function() {
+
+ 		    $(this).addClass("trclick").siblings().removeClass("trclick");
+ 		    for (i = 0; i <= 4; i++) {
+
+ 		        $('.text').eq(i).val($('.trclick > td').eq(i).text());
+ 		    };
+ 		});
+
+ });
+
+ $('#deletebtn').click(function() {
 
      if (confirm("確定刪除？")) {
          $('.trclick').remove();
      };
  });
 
-
-
 });
-
-function trclick() {
-    $('.trnormal').click(function() {
-        $(this).addClass("trclick").siblings().removeClass("trclick");
-        for (i = 0; i <= 4; i++) {
-
-            $('.text').eq(i).val($('.trclick > td').eq(i).text());
-        };
-    });
-};
