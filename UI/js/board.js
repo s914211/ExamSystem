d3c05a627944e94136e418bbc28256cc8f59c4ec
jimxjim <xyz2222aqaq@yahoo.com.tw>
2014-10-22 17:07:14 +0800

    little edit

--- UI/js/board.js
+++ UI/js/board.js
@@ -23,29 +23,35 @@ $('#editbtn').click(function() {
 
 
 $('#newbtn').click(function() {
-    	$('.trnormal').removeClass('trclick');
-    	var tr = $("<tr class=\"trnormal trclick\"	>" 
-        	+ "<td>" + $('.text').eq(0).val() + "</td>" 
-        	+ "<td>" + $('.text').eq(1).val() + "</td>" 
-        	+ "<td>" + $('.text').eq(2).val() + "</td>" 
-        	+ "<td>" + $('.text').eq(3).val() + "</td>" 
-        	+ "<td>" + $('.text').eq(4).val() + "</td>" 
-        	+ "<td class=\"tdsmall\">" + $('#dif').val() + "</td>"
-        	+ "<td class=\"tdsmall\">" + $('#ans').val() + "</td>"
-        	+ "</tr>");
+	if($('#dif').val()=="請選擇" || $('#ans').val()=="請選擇"){
 
-       	$('table').append(tr);
- 		
- 		tr.on('click', function() {
-
- 		    $(this).addClass("trclick").siblings().removeClass("trclick");
- 		    for (i = 0; i <= 4; i++) {
-
- 		        $('.text').eq(i).val($('.trclick > td').eq(i).text());
- 		    };
-    			$('#dif').val($('.trclick > td').eq(5).text());
-   				$('#ans').val($('.trclick > td').eq(6).text()); 		    
- 		});
+	}
+	else
+    	{
+    		$('.trnormal').removeClass('trclick');
+    	    	var tr = $("<tr class=\"trnormal trclick\"	>" 
+    	        	+ "<td>" + $('.text').eq(0).val() + "</td>" 
+    	        	+ "<td>" + $('.text').eq(1).val() + "</td>" 
+    	        	+ "<td>" + $('.text').eq(2).val() + "</td>" 
+    	        	+ "<td>" + $('.text').eq(3).val() + "</td>" 
+    	        	+ "<td>" + $('.text').eq(4).val() + "</td>" 
+    	        	+ "<td class=\"tdsmall\">" + $('#dif').val() + "</td>"
+    	        	+ "<td class=\"tdsmall\">" + $('#ans').val() + "</td>"
+    	        	+ "</tr>");
+    	
+    	       	$('table').append(tr);
+    	 		
+    	 		tr.on('click', function() {
+    	
+    	 		    $(this).addClass("trclick").siblings().removeClass("trclick");
+    	 		    for (i = 0; i <= 4; i++) {
+    	
+    	 		        $('.text').eq(i).val($('.trclick > td').eq(i).text());
+    	 		    };
+    	    			$('#dif').val($('.trclick > td').eq(5).text());
+    	   				$('#ans').val($('.trclick > td').eq(6).text()); 		    
+    	 		});
+    	 	};
 
  });
 
