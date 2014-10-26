3ebe355b46367a11fa2b0d7bc2f697fb43d3945d
jimxjim <xyz2222aqaq@yahoo.com.tw>
2014-10-26 14:27:21 +0800

    test almost

--- UI/js/test.js
+++ UI/js/test.js
@@ -9,40 +9,18 @@ $(document).ready(function() {
 
 
 
-    $('.number').click(function() {
-
+    $('.number,.left,.right').click(function() {
+        for (i = 1; i <= 40; i++) {
+            if (ans[i][6]) {
+                $('.number').eq(i - 1).addClass('ansslecet');
+            }
+        }
         $('.answer').removeClass('answerclick');
         $('.testnumber').text("");
         $('.qusanswer').text("");
         $(this).addClass('numberclick').siblings().removeClass('numberclick');
-        $('.testnumber').text("第" + $(this).text() + "題，" + "你選擇的答案是");
-        if (ans[$('.numberclick').text()][6]) {
-            switch (ans[$('.numberclick').text()][6]) {
-                case "A":
-                    $('.A').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "B":
-                    $('.B').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "C":
-                    $('.C').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "D":
-                    $('.D').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-            }
+        useranswer();
 
-        }
-        for(i = 1; i <= 40; i++)
-	{
-		if(ans[i][6]){
-			$('.number').eq(i-1).addClass('ansslecet');
-		}
-	}
     });
 
     $('.left,.right').width($(window).width() * 0.07);
@@ -52,33 +30,13 @@ $(document).ready(function() {
         if ($('.numberclick').text() == "1") {
             alert('這是第一題');
         } else {
-        	$('.answer').removeClass('answerclick');
-        	$('.testnumber').text("");
-        	$('.qusanswer').text("");
+            $('.answer').removeClass('answerclick');
+            $('.testnumber').text("");
+            $('.qusanswer').text("");
             $('.numberclick').prev().addClass('numbertoken');
             $('.numberclick').removeClass('numberclick');
             $('.numbertoken').addClass('numberclick').removeClass('numbertoken');
-            $('.testnumber').text("第" + $('.numberclick').text() + "題，" + "你選擇的答案是");
-            if (ans[$('.numberclick').text()][6]) {
-            switch (ans[$('.numberclick').text()][6]) {
-                case "A":
-                    $('.A').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "B":
-                    $('.B').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "C":
-                    $('.C').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "D":
-                    $('.D').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-            }
-        }
+            useranswer();
 
 
 
@@ -89,41 +47,21 @@ $(document).ready(function() {
         if ($('.numberclick').text() == "40") {
             alert("已經是最後一題");
         } else {
-        	$('.answer').removeClass('answerclick');
-        	$('.testnumber').text("");
-        	$('.qusanswer').text("");
+            $('.answer').removeClass('answerclick');
+            $('.testnumber').text("");
+            $('.qusanswer').text("");
             $('.numberclick').next().addClass('numbertoken');
             $('.numberclick').removeClass('numberclick');
             $('.numbertoken').addClass('numberclick').removeClass('numbertoken');
-            $('.testnumber').text("第" + $('.numberclick').text() + "題，" + "你選擇的答案是");
-            if (ans[$('.numberclick').text()][6]) {
-            switch (ans[$('.numberclick').text()][6]) {
-                case "A":
-                    $('.A').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "B":
-                    $('.B').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "C":
-                    $('.C').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-                case "D":
-                    $('.D').addClass('answerclick');
-                    $('.qusanswer').text(ans[$('.numberclick').text()][6]);
-                    break;
-            }
-        }
+            useranswer();
 
         }
 
 
 
     });
-	
-	
+
+
 
 
 });
@@ -137,4 +75,29 @@ $(window).resize(function() {
 var ans = new Array();
 for (i = 1; i <= 40; i++) {
     ans[i] = Array(6);
+}
+
+function useranswer() {
+    $('.testnumber').text("第" + $(this).text() + "題，" + "你選擇的答案是");
+    if (ans[$('.numberclick').text()][6]) {
+        switch (ans[$('.numberclick').text()][6]) {
+            case "A":
+                $('.A').addClass('answerclick');
+                $('.qusanswer').text(ans[$('.numberclick').text()][6]);
+                break;
+            case "B":
+                $('.B').addClass('answerclick');
+                $('.qusanswer').text(ans[$('.numberclick').text()][6]);
+                break;
+            case "C":
+                $('.C').addClass('answerclick');
+                $('.qusanswer').text(ans[$('.numberclick').text()][6]);
+                break;
+            case "D":
+                $('.D').addClass('answerclick');
+                $('.qusanswer').text(ans[$('.numberclick').text()][6]);
+                break;
+        }
+
+    }
 }
