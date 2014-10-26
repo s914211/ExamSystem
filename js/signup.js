05616a461164d732ce1bfba93f07c98d9a52e3b9
s914211 <cchccy1623@gmail.com>
2014-10-26 19:49:32 +0800

    signup.js combine with index.html

--- js/signup.js
+++ js/signup.js
@@ -2,20 +2,21 @@ Parse.initialize("c1V2V3BZTN1lPM7G3L8cLNeI8EAV7XnlvOH4F5CG", "6ddAWuezFW3Bg3xOJa
 
 signup = function(){
             
-      if(document.getElementById('password').value != document.getElementById('confirmpassword').value){
+      if(document.getElementById('signuppassword').value != document.getElementById('signupcpassword').value){
             alert('密碼不一樣，請確認密碼是一樣的！！');
       }
       else{
         var user = new Parse.User();
-            user.set("username", document.getElementById('username').value);
+            user.set("userid", document.getElementById('signupid').value);
+            user.set("username", document.getElementById('signupname').value);
             user.set("password", document.getElementById('password').value);
-            user.set("phonenum", document.getElementById('phonenum').value);
-            user.set("email", document.getElementById('email').value);
-            user.set("gender", document.getElementById('gender').value);
+            user.set("phonenum", document.getElementById('signuptel').value);
+            user.set("email", document.getElementById('signupemail').value);
+            user.set("gender", document.getElementById('signupgender').value);
             user.signUp(null, {
                 success: function(user){
                   alert('success!!');
-                  //window.location.assign("LoginPage.html");
+                  window.location.assign("UI/index.html");
                 },
                 error: function(user, error){
                   alert('fail');
@@ -24,7 +25,24 @@ signup = function(){
       }
 };
 
+document.getElementById('signupbtn').addEventListener("click", signup);
+
 login = function(){
+  var userid = document.getElementById('loginid');
+  var userpassword = document.getElementById('loginpassword');
+  Parse.User.logIn(userid,userpassword,{
+    success:function(){
+      alert("login success!");
+    },
+    error:function(){
+      alert("Login failed!");
+    }
+  }
+};
+
+document.getElementById('loginbtn').addEventListener("click", login);
+
+/*login = function(){
     var user = Parse.Object.extend('User');
     var query = new Parse.Query(user);
     query.find({
@@ -46,4 +64,4 @@ login = function(){
             }
         }
     })
-}
+}*/
