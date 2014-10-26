7643e03b3f965bc87875317dc00d5f6fbf855bd9
s914211 <cchccy1623@gmail.com>
2014-10-26 20:29:16 +0800

    last try

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -1,46 +1,51 @@
-Parse.initialize("c1V2V3BZTN1lPM7G3L8cLNeI8EAV7XnlvOH4F5CG", "6ddAWuezFW3Bg3xOJa7ryzTSmMjP3ZB4fYJNFqty");
+$(document).ready(function(){
+  Parse.initialize("c1V2V3BZTN1lPM7G3L8cLNeI8EAV7XnlvOH4F5CG", "6ddAWuezFW3Bg3xOJa7ryzTSmMjP3ZB4fYJNFqty");
 
-signup = function(){
-            
-      if(document.getElementById('signuppassword').value != document.getElementById('signupcpassword').value){
-            alert('密碼不一樣，請確認密碼是一樣的！！');
+  signup = function(){
+              
+        if(document.getElementById('signuppassword').value != document.getElementById('signupcpassword').value){
+              alert('密碼不一樣，請確認密碼是一樣的！！');
+        }
+        else{
+          var user = new Parse.User();
+              user.set("userid", document.getElementById('signupid').value);
+              user.set("username", document.getElementById('signupname').value);
+              user.set("password", document.getElementById('signuppassword').value);
+              user.set("phonenum", document.getElementById('signuptel').value);
+              user.set("email", document.getElementById('signupemail').value);
+              user.set("gender", document.getElementById('signupgender').value);
+              user.signUp(null, {
+                  success: function(user){
+                    alert('success!!');
+                    window.location.assign("UI/index.html");
+                  },
+                  error: function(user, error){
+                    alert('fail');
+                  }
+              });
+        }
+  };
+
+  document.getElementById("signupbtn").addEventListener("click", signup);
+
+  login = function(){
+    var userid = document.getElementById('loginid');
+    var userpassword = document.getElementById('loginpassword');
+    Parse.User.logIn(userid,userpassword,{
+      success:function(){
+        alert("login success!");
+      },
+      error:function(){
+        alert("Login failed!");
       }
-      else{
-        var user = new Parse.User();
-            user.set("userid", document.getElementById('signupid').value);
-            user.set("username", document.getElementById('signupname').value);
-            user.set("password", document.getElementById('password').value);
-            user.set("phonenum", document.getElementById('signuptel').value);
-            user.set("email", document.getElementById('signupemail').value);
-            user.set("gender", document.getElementById('signupgender').value);
-            user.signUp(null, {
-                success: function(user){
-                  alert('success!!');
-                  window.location.assign("UI/index.html");
-                },
-                error: function(user, error){
-                  alert('fail');
-                }
-            });
-      }
-};
+    })
+  };
 
-//document.getElementById("signupbtn").addEventListener("click", signup);
 
-login = function(){
-  var userid = document.getElementById('loginid');
-  var userpassword = document.getElementById('loginpassword');
-  Parse.User.logIn(userid,userpassword,{
-    success:function(){
-      alert("login success!");
-    },
-    error:function(){
-      alert("Login failed!");
-    }
-  })
-};
+  document.getElementById("loginbtn").addEventListener("click", login);
+})
 
-//document.getElementById("loginbtn").addEventListener("click", login);
+
 
 /*login = function(){
     var user = Parse.Object.extend('User');
