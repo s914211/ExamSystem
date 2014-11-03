7986c847d31939381220b15b58c0fd782b960939
jimxjim <xyz2222aqaq@yahoo.com.tw>
2014-11-04 02:53:50 +0800

    stupid method for test,need to improve

--- UI/js/test.js
+++ UI/js/test.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
     Parse.initialize("c1V2V3BZTN1lPM7G3L8cLNeI8EAV7XnlvOH4F5CG", "6ddAWuezFW3Bg3xOJa7ryzTSmMjP3ZB4fYJNFqty");
 
     var question = Parse.Object.extend('example');
-        var query = new Parse.Query(question);
-        query.equalTo('no', '1');
-        query.first({
-            success:function(examquestion){
-                var ques = examquestion.get('Question');
-                var optiona = examquestion.get('OptionA');
-                var optionb = examquestion.get('OptionB');
-                var optionc = examquestion.get('OptionC');
-                var optiond = examquestion.get('OptionD'); 
-                document.getElementById("bigdiv").innerHTML = "<div class='question'> <div class='quscontent'>"+ques + " </div> </div> <div class='content'> <div class='answer A'> <div class='choiceimg'></div>"+optiona+" <span class='choice'>A</span> </div> <div class='answer B'> <div class='choiceimg'></div>"+optionb+" <span class='choice'>B</span> </div> <div class='answer C'><div class='choiceimg'></div>"+optionc+"<span class='choice'>C</span></div><div class='answer D'><div class='choiceimg'></div>"+optiond+"<span class='choice'>D</span></div></div>";
-            }
-        })
+    var query = new Parse.Query(question);
+    query.equalTo('no', '1');
+    query.first({
+        success: function(examquestion) {
+            var ques = examquestion.get('Question');
+            var optiona = examquestion.get('OptionA');
+            var optionb = examquestion.get('OptionB');
+            var optionc = examquestion.get('OptionC');
+            var optiond = examquestion.get('OptionD');
+            document.getElementById("bigdiv").innerHTML = "<div class='question'> <div class='quscontent'>" + ques + " </div> </div> <div class='content'> <div class='answer A'> <div class='choiceimg'></div>" + optiona + " <span class='choice'>A</span> </div> <div class='answer B'> <div class='choiceimg'></div>" + optionb + " <span class='choice'>B</span> </div> <div class='answer C'><div class='choiceimg'></div>" + optionc + "<span class='choice'>C</span></div><div class='answer D'><div class='choiceimg'></div>" + optiond + "<span class='choice'>D</span></div></div>";
+        }
+    })
 
-    $('#bigdiv').delegate('.answer','click',function() {
+    $('#bigdiv').delegate('.answer', 'click', function() {
         $(this).addClass("answerclick").siblings().removeClass("answerclick");
         $(".qusanswer").text($(this).children(".choice").text());
         userans[$('.numberclick').text()][6] = $(this).children('.choice').text();
@@ -23,7 +23,22 @@ $(document).ready(function() {
 
 
 
-    $('body').delegate('.number,.left,.right','click',function() {
+    $('body').delegate('.number,.left,.right', 'click', function() {
+
+        var question = Parse.Object.extend('example');
+        var query = new Parse.Query(question);
+        query.equalTo('no', $('.numberclick').text());
+        query.first({
+            success: function(examquestion) {
+                var ques = examquestion.get('Question');
+                var optiona = examquestion.get('OptionA');
+                var optionb = examquestion.get('OptionB');
+                var optionc = examquestion.get('OptionC');
+                var optiond = examquestion.get('OptionD');
+                document.getElementById("bigdiv").innerHTML = "<div class='question'><div class='quscontent'>" + ques + " </div></div><div class='content'><div class='answer A'><div class='choiceimg'></div>" + optiona + "<span class='choice'>A</span></div><div class='answer B'><div class='choiceimg'></div>" + optionb + "<span class='choice'>B</span></div><div class='answer C'><div class='choiceimg'></div>" + optionc + "<span class='choice'>C</span></div><div class='answer D'><div class='choiceimg'></div>" + optiond + "<span class='choice'>D</span></div></div>";
+            }
+        });
+       
         for (i = 1; i <= 40; i++) {
             if (userans[i][6]) {
                 $('.number').eq(i - 1).addClass('ansslecet');
@@ -33,21 +48,9 @@ $(document).ready(function() {
         $('.testnumber').text("");
         $('.qusanswer').text("");
         $(this).addClass('numberclick').siblings().removeClass('numberclick');
-        useranswer();
+        window.setTimeout(function(){useranswer();},400);
 
-        var question = Parse.Object.extend('example');
-        var query = new Parse.Query(question);
-        query.equalTo('no', $('.numberclick').text());
-        query.first({
-            success:function(examquestion){
-                var ques = examquestion.get('Question');
-                var optiona = examquestion.get('OptionA');
-                var optionb = examquestion.get('OptionB');
-                var optionc = examquestion.get('OptionC');
-                var optiond = examquestion.get('OptionD'); 
-                document.getElementById("bigdiv").innerHTML = "<div class='question'> <div class='quscontent'>"+ ques + " </div> </div> <div class='content'> <div class='answer A'> <div class='choiceimg'></div>"+optiona+" <span class='choice'>A</span> </div> <div class='answer B'> <div class='choiceimg'></div>"+optionb+" <span class='choice'>B</span> </div> <div class='answer C'><div class='choiceimg'></div>"+optionc+"<span class='choice'>C</span></div><div class='answer D'><div class='choiceimg'></div>"+optiond+"<span class='choice'>D</span></div></div>";
-            }
-        });
+
 
 
     });
@@ -55,7 +58,7 @@ $(document).ready(function() {
     $('.left,.right').width($(window).width() * 0.07);
     $('.left,.right').height($(window).width() * 0.07);
 
-    $('.arrow').delegate('.left','click',function() {
+    $('.arrow').delegate('.left', 'click', function() {
         if ($('.numberclick').text() == "1") {
             alert('這是第一題');
         } else {
@@ -65,7 +68,7 @@ $(document).ready(function() {
             $('.numberclick').prev().addClass('numbertoken');
             $('.numberclick').removeClass('numberclick');
             $('.numbertoken').addClass('numberclick').removeClass('numbertoken');
-            useranswer();
+
 
 
 
@@ -73,7 +76,7 @@ $(document).ready(function() {
         }
     });
 
-    $('.arrow').delegate('.right','click',function() {
+    $('.arrow').delegate('.right', 'click', function() {
         if ($('.numberclick').text() == "40") {
             alert("已經是最後一題");
         } else {
@@ -83,7 +86,7 @@ $(document).ready(function() {
             $('.numberclick').next().addClass('numbertoken');
             $('.numberclick').removeClass('numberclick');
             $('.numbertoken').addClass('numberclick').removeClass('numbertoken');
-            useranswer();
+
 
 
         }
