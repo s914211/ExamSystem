52797f34a7069505059512238bacc1bda409b411
ChenYuHsin <s63232@hotmail.com>
2014-11-17 23:40:00 +0800

    test optimization

--- js/test.js
+++ js/test.js
@@ -1,31 +1,42 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // =================================================================pagination
 
-$('a').on('click', function(e){
-  $(e).preventDefault();
-});
 
-$('.pagination li').on('click', function(){
-    console.log($(this));
-  $(this).siblings().removeClass('active');
-  $(this).addClass('active');
-  
+
+
+
+
+
+$('.pagination li').on('click', function(e){
+  $(".pagination li").removeClass('active');
+  $(this).addClass('active');  
 })
+
+
+
+// =================================================================pagination-arrows
+
+var pr = document.querySelector( '.paginate.left' );
+var pl = document.querySelector( '.paginate.right' );
+
+pr.onclick = slide.bind( this, -1 );
+pl.onclick = slide.bind( this, 1 );
+
+var index = 0, total = 5;
+
+function slide(offset) {
+  index = Math.min( Math.max( index + offset, 0 ), total - 1 );
+
+  document.querySelector( '.counter' ).innerHTML = ( index + 1 ) + ' / ' + total;
+
+  pr.setAttribute( 'data-state', index === 0 ? 'disabled' : '' );
+  pl.setAttribute( 'data-state', index === total - 1 ? 'disabled' : '' );
+}
+
+slide(0);
+
+
+
 
 
 
