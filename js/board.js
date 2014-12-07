0635cdeba5f54269e5aa184827d2c170e1612a94
ChenYuHsin <s63232@hotmail.com>
2014-12-07 11:03:59 +0800

    fix student style

--- js/board.js
+++ js/board.js
@@ -278,8 +278,25 @@ $(document).bind('click', function(e){
 });
 
 
+// back to the top============================================
 
-
+$(document).ready(function() {
+            // Show or hide the sticky footer button
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 200) {
+                    $('.go-top').fadeIn(200);
+                } else {
+                    $('.go-top').fadeOut(200);
+                }
+            });
+            
+            // Animate the scroll to top
+            $('.go-top').click(function(event) {
+                event.preventDefault();
+                
+                $('html, body').animate({scrollTop: 0}, 300);
+            })
+        });
 
 
 
